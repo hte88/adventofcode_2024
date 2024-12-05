4536dc1ffae709736b0c95d5d4ac15ca9da7000c
hte88 <olivier.marion88@gmail.com>
2024-12-05 10:32:27 +0100

    feat: step 3 part 2 valid

--- src/Day_03/step_2.ts
+++ src/Day_03/step_2.ts
@@ -1,62 +1,43 @@
 const decoder: TextDecoder = new TextDecoder('utf-8');
-
 const filePath: string = new URL('./input.txt', import.meta.url).pathname;
 
 try {
   const data: Uint8Array = await Deno.readFile(filePath);
   const fileContent: string = decoder.decode(data);
-
-  const rows: string = fileContent.trim();
+  let content: string = fileContent.trim();
 
   const totalValidReports: number[] = [];
 
-  const l = rows.split(`don't()`);
+  const countDo = (content.match(/do\(\)/g) || []).length;
+  const countDont = (content.match(/don't\(\)/g) || []).length;
 
-  console.log(l);
-  const r = [];
-  for (let index = 0; index < l.length; index++) {
-    const element = l[index];
+  let extractedSegments = '';
 
-    if (element.search('do()') > -1) {
-      const lt = element.split('do()');
-      if (lt.length > 0) {
-        r.push(`don't()${lt[0]}do()`);
-      }
+  for (let index = 0; index < countDont; index++) {
+    const indexDont = content.indexOf(`don't()`);
+    const indexDo = content.indexOf(`do()`);
+
+    if (indexDont > -1 && indexDont < indexDo) {
+      const segmentToRemove = content.slice(indexDont, indexDo + 4);
+      content = content.replace(segmentToRemove, '');
+    } else if (indexDont > indexDo) {
+      const segmentToKeep = content.slice(indexDo, indexDont);
+      extractedSegments += segmentToKeep;
+      content = content.replace(segmentToKeep, '');
+    } else {
+      break;
     }
   }
 
-  let ppp = '';
-  for (let index = 0; index < r.length; index++) {
-    const element = r[index];
-    ppp = rows.replaceAll(element, '');
-  }
+  const processedContent = extractedSegments + content;
 
-  let pute = ppp;
-  if (ppp.search(`don't()`) > -1) {
-    const p = ppp.split(`don't()`);
-
-    pute = p[0];
-  }
   const regex = /mul\((\d+),(\d+)\)/g;
-
-  for (const match of pute.matchAll(regex)) {
+  for (const match of processedContent.matchAll(regex)) {
     totalValidReports.push(Number(match[1]) * Number(match[2]));
   }
-  /* const re: string = r.toString();
-  if (re.search(`don't()`)) {
-    re.split(`don't()`);
-    console.log('re', re);
-  }
-  const regex = /mul\((\d+),(\d+)\)/g;
 
-  for (const match of rows.matchAll(regex)) {
-    totalValidReports.push(Number(match[1]) * Number(match[2]));
-  } */
-
-  console.log(
-    'Total valid reports:',
-    totalValidReports.reduce((a, b) => a + b, 0)
-  );
+  const total = totalValidReports.reduce((a, b) => a + b, 0);
+  console.log('Total des produits valides: ', total);
 } catch (error) {
-  console.error('Error reading the file:', error.message);
+  console.error('Erreur lors de la lecture du fichier: ', error.message);
 }
