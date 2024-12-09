c5572ba283a81d584f7bfd2e3afe7692b0111a71
hte88 <olivier.marion88@gmail.com>
2024-12-09 22:27:37 +0100

    feat: Day 7 test combinaison

--- src/Day_07/step_1.ts
+++ src/Day_07/step_1.ts
@@ -20,31 +20,51 @@ function generateCombination(
   return combination;
 }
 
+function joinCombination(numbers: number[], combination: string[]): string {
+  let calcul = '';
+  for (let index = 0; index < numbers.length; index++) {
+    const element: number = numbers[index];
+    const operator: string = combination[index] ?? '';
+    calcul += `${element}${operator}`;
+  }
+
+  return calcul;
+}
+
 try {
   const data: Uint8Array = await Deno.readFile(filePath);
   const fileContent: string = decoder.decode(data);
   const content: string[] = fileContent.split('\n').slice(0, -1) ?? [];
 
-  console.log(content);
-
   const operators: string[] = ['+', '*'];
+  const ok = [];
+  const ko = [];
 
   for (let index = 0; index < content.length; index++) {
     const row = content[index];
     const [result, numbersString] = row.split(':');
-    const numbers = numbersString.trim().split(' ');
+    const numbers = numbersString.trim().split(' ').map(Number);
     const numOperators = numbers.length - 1;
     const totalCombinations = Math.pow(operators.length, numOperators);
 
-    console.log('numbers =', numbers);
+    /*   console.log('numbers =', numbers);
     console.log('posibilite =', numOperators);
-    console.log('totalCombinations =', totalCombinations);
+    console.log('totalCombinations =', totalCombinations); */
 
     for (let i = 0; i < totalCombinations; i++) {
       const combination = generateCombination(i, numOperators, operators);
-      console.log(combination);
+      const calcul = joinCombination(numbers, combination);
+      console.log(eval(calcul), calcul, Number(result));
+
+      if (eval(calcul) === Number(result)) {
+        ok.push({ result: Number(result), combination: calcul, numbers });
+      } else {
+        ko.push({ result: Number(result), combination: calcul, numbers });
+      }
     }
   }
+
+  //console.log(ok, ko);
 
   const totalOccurrences = [0];
 
